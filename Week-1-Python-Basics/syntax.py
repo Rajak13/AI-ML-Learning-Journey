848b1d18cd50cb077093920c5484fde9648e57c7
Rajak13 <queresma05@gmail.com>
2025-01-23 18:56:34 +0545

    Added python-cheat-sheet and information about syntax in Python.

---
+++ Week-1-Python-Basics/syntax.py
@@ -0,0 +1,31 @@
+# Python Syntax Basics
+
+def our_function(): #Function Declaration
+    print("Hello World") #Printing a sentence.
+    
+our_function() #Calling the function
+
+#Output: Hello World
+
+a_string = "Hello World" #Assigning a string to a variable using double quotes
+a1_string = 'Hello World' #Assigning a string to a variable using single quotes
+
+print(a_string) #Printing the variable with double quotes
+print(a1_string) #Printing the variable with single quotes
+
+# Long Statements
+# You can use the backslash to continue a statement on the next line. This is useful for debugging and readability.
+if(12 < 13) and (13 > 12) and \
+    (12 == 12):
+    print("True")
+
+#Output: True
+
+# Casing
+# Camel Case
+myVariableName = "Hello World"
+# Pascal Case
+MyVariableName = "Hello World" #Used for class names
+# Snake Case
+my_variable_name = "Hello World" #Used for variable,function and module names
+
